044335d954e4d65e245e2932d117094a5f3f30a4
01Clarian <clarionnorth@gmail.com>
2019-11-10 18:07:34 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import Grid from '@material-ui/core/Grid';
 import './App.css';
+import PplCard from './PplCard';
 
+// setting your users initial state with React Hook
 function App() {
+  const [users, setUsers] = useState([])
+
+//setting up our async await fetch and turing response to json
+  useEffect(()=>{
+    async function fetchData() {
+        setUsers(
+      await fetch('https://reqres.in/api/users?page=2')
+          .then(res => res.json())
+          .then(res => res.data)
+          .catch(err => console.log(err, 'warning'))
+        )
+    }
+    fetchData();
+  } 
+  ,[])
+
+  //setting up our nested grids from material ui
+  //outer grid is the container, the inner grid sets up the items
+  
+  //we iterate through the inner grid into the ppl card to gain control  
+  //PplCard propogated with API data. 
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <h3>THE TRUE BEAUTY OF MATERIAL UI</h3>
+        <Grid
+        container
+        spacing={10}
+        style={{padding: '24px'}}
         >
-          Learn React
-        </a>
-      </header>
+        {users.map( users =>  
+          <Grid
+          key={users.id}
+          item
+          xs={12} sm={6} md={4} lg={4} xl={3}
+        >
+          <PplCard
+          key={users.id}
+          email={users.email}
+          firstname={users.first_name}
+          lastname={users.last_name}
+          avatar={users.avatar}
+          />
+        </Grid>
+        )}
+        </Grid>
     </div>
   );
 }
---
+++ src/PplCard.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+// import material ui smaller components to create card component
+
+// makeStyles for material ui
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
+
+// customize the media card from material ui to suit our user cards.
+export default function PplCard({email, firstname, lastname, avatar}) {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={avatar}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {firstname + " " + lastname}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {email}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button href={avatar} size="small" color="primary">
+          Thumbnail Pic
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
